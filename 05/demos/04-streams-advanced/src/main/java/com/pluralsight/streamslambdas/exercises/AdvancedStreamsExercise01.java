b9e13e795b885a7dc264d8acd9902298759af9a3
Sascha Homeier <shomeier@apollon.de>
2021-09-05 16:02:56 +0700

    AdvancedStreams - Exercise 01

--- 05/demos/04-streams-advanced/src/main/java/com/pluralsight/streamslambdas/exercises/AdvancedStreamsExercise01.java
+++ 05/demos/04-streams-advanced/src/main/java/com/pluralsight/streamslambdas/exercises/AdvancedStreamsExercise01.java
@@ -1,9 +1,10 @@
 package com.pluralsight.streamslambdas.exercises;
 
-import com.pluralsight.streamslambdas.Product;
-
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import com.pluralsight.streamslambdas.Product;
 
 public class AdvancedStreamsExercise01 {
 
@@ -17,12 +18,18 @@ public class AdvancedStreamsExercise01 {
         // TODO: Collect the names of the products into a TreeSet.
         //
         // Hint: Use the collect() method that takes three functions parameters.
-        // What is the purpose of each of these three functions and how do you implement them? (Consult the API documentation).
+        // What is the purpose of each of these three functions and how do you implement them? (Consult the
+        // API documentation).
         // Use method references or lambda expressions to implement the three functions.
 
-//        return products.stream()
-//                .collect(...);
+        // return products.stream()
+        // .collect(...);
 
-        throw new UnsupportedOperationException("Not yet implemented"); // Remove this line
+        return products.stream()
+                .map(Product::getName)
+                .sorted()
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        // throw new UnsupportedOperationException("Not yet implemented"); // Remove this line
     }
 }
